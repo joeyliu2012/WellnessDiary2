54305f7f083b45a1f7565dafa814a502507a73d9
Zach Perrault <zach.perrault@gmail.com>
2016-04-20 08:02:00 -0400

    removes sleep

--- ios/WellnessDiary2/AppDelegate.m
+++ ios/WellnessDiary2/AppDelegate.m
@@ -33,7 +33,7 @@
    * on the same Wi-Fi network.
    */
 
-  //jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -50,7 +50,7 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 
-  sleep(3);
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
